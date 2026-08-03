0e636c6ec9e14ae587b9d21c6bfdc391df896e66
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2026-08-03 13:34:34 +0530

    WHERE operations

--- E-commerce Database.sql
+++ E-commerce Database.sql
@@ -1,24 +1,30 @@
-SELECT * FROM products ;
+SELECT * FROM customers 
+WHERE city = 'Delhi' ;
 
-SELECT name, city FROM customers ;
+SELECT * FROM products
+WHERE price > 30000 ;
 
-SELECT product, price FROM products ;
+SELECT * FROM orders 
+WHERE quantity > 1 ;
 
-SELECT * FROM orders ;
+SELECT * FROM products
+WHERE category = 'Electronics' AND
+price > 20000 ;
 
-SELECT DISTINCT city FROM customers ;
+SELECT * FROM customers
+WHERE city IN('Delhi','Patna') ;
 
-SELECT product, price FROM products 
-ORDER BY price DESC LIMIT 3 ;
+SELECT * FROM customers
+WHERE age BETWEEN 25 AND 35 ;
 
-SELECT product, price FROM products 
-ORDER BY price ASC LIMIT 2 ;
+SELECT * FROM products 
+WHERE product LIKE 'H%' ;
 
-SELECT product FROM products 
-ORDER BY price ASC ;
+SELECT * FROM customers
+WHERE name LIKE '%a' ;
 
-SELECT product, category, price FROM products
-ORDER BY price DESC ;
+SELECT * FROM products 
+WHERE price BETWEEN 2500 AND 10000 ;
 
-SELECT name FROM customers
-ORDER BY name DESC;
+SELECT * FROM customers
+WHERE city != 'Delhi' ;
