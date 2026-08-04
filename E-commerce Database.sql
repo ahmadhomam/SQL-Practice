83603da46cab6babe02f66f3a02af0203d25a9b6
Homam Ahmad Kazimi <ahmadhomam38@gmail.com>
2026-08-04 22:49:14 +0530

    New concept of COUNT(column) : pass the column that contain null things

--- E-commerce Database.sql
+++ E-commerce Database.sql
@@ -66,3 +66,40 @@ FROM customers AS c
 LEFT JOIN orders AS o
 	ON c.customer_id  =  o.customer_id ;
 
+
+SELECT c.customer_id,
+	   c.name
+FROM customers AS c
+LEFT JOIN orders AS o
+	ON c.customer_id  =  o.customer_id 
+WHERE o.order_id IS NULL ;
+
+INSERT INTO products(product_id,product,category,price)
+VALUES(106,'Tablet','Electronics',45000)
+
+SELECT p.product_id,
+	   p.product,
+	   p.price
+FROM products AS p
+LEFT JOIN orders AS o
+	ON p.product_id = o.product_id
+WHERE o.product_id IS NULL ;
+
+SELECT p.product_id,
+	   p.product,
+	   COUNT(o.order_id) AS order_count
+FROM products AS p
+LEFT JOIN orders AS o
+	ON o.product_id = p.product_id
+GROUP BY p.product_id,
+		 p.product ; 
+
+SELECT c.name,
+	   COUNT(o.order_id) AS total_order
+FROM customers AS c
+LEFT JOIN orders AS o
+	ON c.customer_id = o.customer_id
+GROUP BY c.customer_id,
+		 c.name  ;
+
+
